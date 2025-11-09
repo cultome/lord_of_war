98ac011f73cbf41371f8ec4e296bec3dfae0f5f9
cultome <csoria@cultome.io>
2025-11-09 01:41:45 -0600

    Improve pagination component

--- lib/lord_of_war/pagination.rb
+++ lib/lord_of_war/pagination.rb
@@ -1,10 +1,11 @@
 class LordOfWar::Pagination
-  attr_reader :page_size, :page
+  attr_reader :page_size, :page, :pages_in_pagination
   attr_accessor :page, :total_records
 
   def initialize(page)
     @page = page.blank? ? 1 : page.to_i
     @page_size = 48
+    @pages_in_pagination = 4
     @total_records = 0
   end
 
@@ -22,9 +23,29 @@ class LordOfWar::Pagination
     @total_records > 0 && @page <= @total_records / @page_size
   end
 
+  def last_page
+    (@total_records / @page_size) + 1
+  end
+
   def pages
-    ((@total_records / @page_size) + 1).times.map do |idx|
-      [idx + 1, idx + 1 == @page]
+    max = [page + (pages_in_pagination / 2), last_page].min
+    min = [page - (pages_in_pagination / 2), 1].max
+
+    middle_pages = (min..max)
+    res = middle_pages.map do |idx|
+      [idx, idx == @page, false]
     end
+
+    unless middle_pages.include? 1
+      res.unshift [1, false, true]
+      res.unshift [1, false, false]
+    end
+
+    unless middle_pages.include? last_page
+      res.push [last_page, false, true]
+      res.push [last_page, false, false]
+    end
+
+    res
   end
 end
